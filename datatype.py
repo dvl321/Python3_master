5195e2c109c9eaae20478e3127817524c4a008ed
Jay <dvlshukla@gmail.com>
2021-06-16 04:45:30 +0530

    datatype

---
+++ datatype.py
@@ -0,0 +1,24 @@
+#text datatype string str
+a = "Hello,This is my first string added for variable"
+print(a)
+print(type(a))
+#number datatype integer int
+
+b = 20
+print(b)
+print(type(b))
+#number datatype floating float 
+
+c = 50.12
+print(c)
+print(type(c))
+
+e = c+b
+print(e)
+print(type(e))
+
+#number with string  datatype choice
+
+d = 7j
+print(d)
+print(type(d))
