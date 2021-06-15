ec512518ad4556dbb1b35c88d715653b4d280b92
Jay <dvlshukla@gmail.com>
2021-06-15 14:53:13 +0530

    syntax file updated with if example

--- syntax.py
+++ syntax.py
@@ -1,3 +1,8 @@
 print("Hello python this is my first check with Vscode")
 print(5+20*3)
 print("We are checking it for syntax")
+if 9 > 2:
+    print("Nine is grater than two")
+
+if 10 < 3:
+    print("Ten is lessthan three")
