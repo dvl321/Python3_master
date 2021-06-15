d0e9ea2150a0e45ceeec6a0c7202dd02d5180ecb
Jay <dvlshukla@gmail.com>
2021-06-15 14:50:24 +0530

    comments

---
+++ comments.py
@@ -0,0 +1,12 @@
+#This is the comment we have added now
+
+print("Hello world")
+print("India boy going to visit italy")
+#print("We are testing now with all the things")
+
+#We add multiple comment using below comment.
+
+'''Multiple comment
+added using this sentax'''
+
+
