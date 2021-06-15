bcd4dd4475dc96e01d956e17979af83d839a913b
Jay <dvlshukla@gmail.com>
2021-06-16 05:03:34 +0530

    Updated syntax

--- syntax.py
+++ syntax.py
@@ -1,8 +1,17 @@
 print("Hello python this is my first check with Vscode")
+
+
 print(5+20*3)
+
+
 print("We are checking it for syntax")
+
+
 if 9 > 2:
     print("Nine is grater than two")
 
+
 if 3 < 10:
     print("three is lessthan Ten")
+
+    
