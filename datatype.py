d21b5b3592082121f5e7d6175b42a92c19bf4ebd
Jay <dvlshukla@gmail.com>
2021-06-17 20:37:45 +0530

    Datatype file updated with all datatype(Text,Numeric,Sequence,Mappin,Boolean,Binary)

--- datatype.py
+++ datatype.py
@@ -22,3 +22,72 @@ print(type(e))
 d = 7j
 print(d)
 print(type(d))
+
+#sequence type: list , tuple and range
+
+#list
+
+f = ["windows","macos","linux"]
+print(f)
+print(type(f))
+
+#tuple
+
+g = ("windows","macos","linux")
+print(g)
+print(type(g))
+
+#range
+
+h = range(10)
+print(h)
+print(type(h))
+
+i = { "name" : "josh","age" : 35 }
+print(i)
+print(type(i))
+
+#set type : set,frozenset
+
+#set
+
+j = {"windows","macos","linux"}
+print(j)
+print(type(j))
+
+#frozenset
+
+k = ({"windows","macos","linux"})
+print(k)
+print(type(k))
+
+#boolean : bool
+
+L = True
+print(L)
+
+M = False
+print(M)
+
+print(type(M)(L))
+
+#sinary type: bytes,bytearry,memoryview
+
+#byte:
+
+n = b"Hello"
+print(n)
+print(type(n))
+
+#bytearray
+
+o = bytearray(1)
+print(o)
+print(type(o))
+
+#memoryview
+
+p = memoryview(bytes(8))
+print(p)
+print(type(p))
+
