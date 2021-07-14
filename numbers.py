85a1f3b6f6ee41e1980d776c7b32c453486028b0
Jay <dvlshukla@gmail.com>
2021-07-14 13:44:25 +0530

    Numbers file added

---
+++ numbers.py
@@ -0,0 +1,104 @@
+#Python number
+#int
+#float
+#complex
+
+'''a = 1   #int
+print(a)
+print(type(a))
+
+b = 3.7 #float
+print(b)
+print(type(b))
+
+c = 1j
+print(c)
+print(type(c))
+
+d = 1
+e = 5467334690235
+f = -456890232
+print(d)
+print(type(d))
+
+print(e)
+print(type(e))
+
+print(f)
+print(type(f))
+
+g = 7.34
+h = 3.0
+i = -58.48
+
+print(g)
+print(type(g))
+
+print(h)
+print(type(h))
+
+print(i)
+print(type(i))
+
+j = 37e8
+k = 24E9
+l = -85.29e120
+
+print(j)
+print(type(j))
+
+print(k)
+print(print(k))
+
+print(l)
+print(print(l))
+
+m = 7+3j
+n = 3j
+o = -2j
+
+print(m)
+print(type(m))
+
+print(n)
+print(type(n))
+
+print(o)
+print(type(o))
+
+
+#Convert from int to float
+p = float(2)
+
+
+#Convert from float to int
+q = int(3.6)
+
+
+#Convert from int to complex
+r = complex(p)
+
+print(type(p))
+print(type(q))
+print(type(r))
+print(r)
+print(q)
+
+
+#Random Numbers'''
+
+import random
+print(random.randrange(1,5))
+
+
+
+
+
+
+
+
+
+
+
+
+
