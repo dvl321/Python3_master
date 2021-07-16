5a12310ca6cbaa3c8127b47671b15ddcd2a31613
Jay <dvlshukla@gmail.com>
2021-07-16 16:06:08 +0530

    casting has been added

---
+++ casting.py
@@ -0,0 +1,33 @@
+#int()
+
+a = int(1)
+b = int(3.7)
+c = int(55.6)
+d = int("6")
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+#float
+e = float(2)
+f = float(45.00)
+g = float("7.3")
+h = float("44.1")
+
+print(e)
+print(f)
+print(g)
+print(h)
+
+#str
+
+i = str("55.7")
+j = str(4)
+k = str(5.0)
+
+print(i)
+print(j)
+print(k)
