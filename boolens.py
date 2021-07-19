c1d9077088ce34f14ca1dee873493d3e1a5dea79
Jay <dvlshukla@gmail.com>
2021-07-19 09:00:04 +0530

    Boolens has been added

---
+++ boolens.py
@@ -0,0 +1,38 @@
+'''print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 300
+b = 44
+
+if b < a:
+    print("a is greater than b")
+
+else:
+    print("b is not greater than a")
+
+print(bool("Hello"))
+
+print(bool(30))
+
+print(bool("Jes"))
+print(bool(123))
+print(bool(["windows,mac,linux"]))
+print(bool(""))
+print(bool(0))
+print(print([]))'''
+
+
+'''def sampleFunction():
+    return True
+
+print(sampleFunction())'''
+
+def  itsmyfunction():
+    return True
+
+if itsmyfunction():
+    print("Yes!")
+
+else:
+    print("NO!")
